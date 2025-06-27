3331bef9629efb7b9bee9ebfdffa154fffd39450
KimDohwan24 <94787456+KimDohwan24@users.noreply.github.com>
2025-06-27 20:37:13 +0900

    fit : Lv.7 N+1 문제 해결

--- src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
@@ -13,4 +13,11 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     @EntityGraph(attributePaths = "user")
     @Query("SELECT c FROM Comment c JOIN c.user WHERE c.todo.id = :todoId")
     List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);
+
+    /*
+                              둘다 똑같이 작동함
+    @Query("SELECT c FROM Comment c JOIN FETCH c.user WHERE c.todo.id = :todoId")
+    List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);
+
+     */
 }
